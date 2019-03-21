0194e52ee0c764e32c8a640b0986049ad8995177
dylan upchr <dylan.upchr@eduge.ch>
2019-03-21 15:46:50 +0100

    sidenav-content

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,7 +9,8 @@ import {
   MatSidenavModule,
   MatListModule,
   MatDividerModule,
-  MatSnackBarModule
+  MatSnackBarModule,
+  MatExpansionModule
 } from '@angular/material';
 
 @NgModule({
@@ -22,7 +23,8 @@ import {
     MatSidenavModule,
     MatListModule,
     MatDividerModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatExpansionModule
   ],
   exports: [
     MatButtonModule,
@@ -33,7 +35,8 @@ import {
     MatSidenavModule,
     MatListModule,
     MatDividerModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatExpansionModule
   ]
 })
 export class MaterialModule {}
