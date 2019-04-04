525569423bcdd25014cf9eea49312f5f597c49ae
root <root@UPCHURCH.lab.ecinf.ch>
2019-04-04 15:25:11 +0200

    file view toolbar buttons

--- src/app/components/file-view/file-view.component.ts
+++ src/app/components/file-view/file-view.component.ts
@@ -8,11 +8,17 @@ import { UserService } from '../../services/user.service';
   styleUrls: ['./file-view.component.scss']
 })
 export class FileViewComponent implements OnInit {
+  sortMenu = [
+    {text: 'test'}
+  ];
+  groupMenu = [
+    {text: 'test'}
+  ];
   toolbarButtons = [
-    { icon: 'filter_list', tooltip: 'Group By'},
-    { icon: 'sort', tooltip: 'Sort By' },
-    { icon: 'view_module', tooltip: 'Grid view' },
-    { icon: 'view_list', tooltip: 'List view' }
+    { icon: 'reorder', tooltip: 'Group By', menu: this.groupMenu},
+    { icon: 'sort', tooltip: 'Sort By', menu: this.sortMenu },
+    { icon: 'view_module', tooltip: 'Grid view', menu: false },
+    { icon: 'view_list', tooltip: 'List view', menu: false }
   ];
 
   constructor(private userService: UserService, private router: Router) {}
