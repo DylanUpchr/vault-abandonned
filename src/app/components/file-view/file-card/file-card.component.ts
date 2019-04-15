7af3ad09cd2125d6eb8af27f3181ac5b6dfd0164
dylan upchr <dylan.upchr@eduge.ch>
2019-04-15 15:50:31 +0200

    file card

---
+++ src/app/components/file-view/file-card/file-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-file-card',
+  templateUrl: './file-card.component.html',
+  styleUrls: ['./file-card.component.scss']
+})
+export class FileCardComponent implements OnInit {
+  @Input() file;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/file-view/file-view.component.ts
+++ src/app/components/file-view/file-view.component.ts
@@ -1,3 +1,4 @@
+import { FileService } from './../../services/file.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
@@ -9,17 +10,19 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./file-view.component.scss']
 })
 export class FileViewComponent implements OnInit {
+  // ---- Toolbar Button Lists -------------------------------------------------------------------
   directionToggle = [
-    {text: 'ascending', value: 'asc', icon: 'arrow_upward'},
-    {text: 'descending', icon: 'arrow_downward'}
+    {text: 'ascending', value: 'asc', icon: 'arrow_upward', checked: false },
+    {text: 'descending', value: 'desc', icon: 'arrow_downward', checked: true }
   ];
   sortMenu = [
-    {text: 'name', icon: 'sort_by_alpha'},
-    {text: 'date', icon: 'calendar_today'},
-    {text: 'size', icon: 'data_usage'}
+    {text: 'name', icon: 'sort_by_alpha', checked: true },
+    {text: 'date', icon: 'calendar_today', checked: false },
+    {text: 'size', icon: 'data_usage', checked: false }
   ];
   groupMenu = [
-    {text: 'test'}
+    {text: 'date', icon: 'calendar_today', checked: false },
+    {text: 'type', icon: 'insert_drive_file', checked: false }
   ];
   toolbarButtons = [
     { icon: 'reorder', tooltip: 'Group By', typeMenu: this.groupMenu, directionMenu: this.directionToggle },
@@ -33,6 +36,8 @@ export class FileViewComponent implements OnInit {
   });
 
 
+  // ---- Files ---------------------------------------------------------------------
+  files: object;
 
   onKeydown(event) {
     const inputSearch = document.getElementById('inputSearch');
@@ -44,14 +49,21 @@ export class FileViewComponent implements OnInit {
     console.log(this.searchFormGroup.value.inputSearch);
   }
 
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(private userService: UserService,
+              private router: Router,
+              private fileService: FileService) {}
 
 
   ngOnInit() {
+    // ---- Logged in ---------------------------------------------------------------------
     this.userService.isLoggedIn().subscribe(loggedIn => {
       if (!loggedIn) {
         this.router.navigate(['/login']);
       }
+    });
+    // ---- Files ---------------------------------------------------------------------
+    this.fileService.getFiles().subscribe(files => {
+      this.files = files;
     });
   }
 }
